5e48a483e16f0263daa4d494bcd4965aa5e21605
Shavem <73134915+Shavem@users.noreply.github.com>
2023-01-19 16:46:45 -0600

    Added some basic boom controls without mapping any ID's yet

--- robot/robot.py
+++ robot/robot.py
@@ -2,7 +2,7 @@
 This file runs the main loop of the robot and is responsible for connecting all other parts of the robot code together.
 
 -----Usage-----
-python py/robot/robot.py deploy --skip-tests
+python robot/robot.py deploy --skip-tests
 # Need to update this to make it automatically launch a .sh file
 
 
@@ -28,4 +28,49 @@ py -3 -m robotpy_installer download robotpy[rev]
 py -3 -m robotpy_installer install robotpy[rev]
 """
 
-# MagicBot framework
+# Import all the necessary libraries
+import wpilib
+import wpilib.drive
+import rev
+
+
+
+
+class MyRobot(wpilib.TimedRobot):
+    """
+    Main framework object that inherits from wpilib.TimedRobot
+    """
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+
+        self.controller = wpilib.XboxController(0)
+
+        self.boom_motor = rev.CANSparkMax(0, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushed)
+        self.extender_motor = rev.CANSparkMax(1, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushed)
+
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        # trust auton work
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+        # this will def make auton actually work
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+
+        # Print both controller values
+        self.X = self.controller.getLeftTriggerAxis()
+        self.Y = self.controller.getRightTriggerAxis()
+
+        print(f"Left Trigger: {self.X}\nRight Trigger: {self.Y}")
+
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
