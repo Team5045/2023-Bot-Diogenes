696f2ee37081d9c3ca9d8187f9d85ebe3d9cf8e1
Pronkle <allenleiguo@gmail.com>
2023-03-09 15:11:59 -0600

    Added SD

--- robot/utilities/encoder.py
+++ robot/utilities/encoder.py
@@ -2,26 +2,39 @@ import wpilib
 import ctre
 from ctre import TalonSRX
 from components.drivetrain import DriveTrain
+import networktables
+from networktables import NetworkTable
 
 talon_ENC1 = TalonSRX(0)
 talon_ENC2 = TalonSRX(12)
 
 class Encoder():
+    sd: NetworkTable 
+    def setup(self):
+        self.sensor1 = talon_ENC1.getSensorCollection()
+        self.sensor2 = talon_ENC2.getSensorCollection()
+
     def getEncoderLeft(self):
+        self.sd.putValue("Event: ", "Getting LEFT Encoder Values")
         print("Left Side Values..")
-        self.sensor = talon_ENC1.getSensorCollection()
-        MotorPosL = self.sensor.getQuadraturePosition()
+
+        MotorPosL = self.sensor1.getQuadraturePosition()
         print(MotorPosL)
-        MotorVelL = self.sensor.getQuadratureVelocity()
+
+        MotorVelL = self.sensor1.getQuadratureVelocity()
         print(MotorVelL)
+        return()
 
     def getEncoderRight(self):
+        self.sd.putValue("Event: ", "Getting RIGHT Encoder Values")
         print("Right Side Values..")
-        self.sense = talon_ENC2.getSensorCollection()
-        MotorPosR = self.sense.getQuadraturePosition()
+
+        MotorPosR = self.sensor2.getQuadraturePosition()
         print(MotorPosR)
-        MotorVelR = self.sense.getQuadratureVelocity
+
+        MotorVelR = self.sensor2.getQuadratureVelocity
         print(MotorVelR)
+        return()
 
 
 
