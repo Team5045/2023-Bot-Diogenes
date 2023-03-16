b0655fbd7746377fa91f0910e8c50a12851df7d8
Jason Niu <corpulentstickerape@gmail.com>
2023-03-16 17:19:06 -0500

    talon brake

--- robot/robot.py
+++ robot/robot.py
@@ -64,16 +64,13 @@ class SpartaBot(MagicRobot):
         NetworkTables.initialize(server='roborio-5045-frc.local')
         self.sd: NetworkTable = NetworkTables.getTable('SmartDashboard')
 
-        self.drive_controller: wpilib.XboxController = wpilib.XboxController(
-            0)  # 0 works for sim?
+        self.drive_controller: wpilib.XboxController = wpilib.XboxController(0)  # 0 works for sim?
 
         self.talon_L_1 = WPI_TalonFX(4)
         self.talon_L_2 = WPI_TalonFX(8)
 
         self.talon_R_1 = WPI_TalonFX(7)
         self.talon_R_2 = WPI_TalonFX(6)
-
-        self.boom_rotator_spark = WPI_TalonFX(3)
 
         self.compressor: wpilib.Compressor = wpilib.Compressor(0, PNEUMATICS_MODULE_TYPE)
 
@@ -84,7 +81,12 @@ class SpartaBot(MagicRobot):
         self.solenoid_gear.set(DoubleSolenoid.Value.kForward)
 
         self.boom_extender_spark: rev.CANSparkMax = rev.CANSparkMax(4, MOTOR_BRUSHLESS)
-        # self.boom_rotator_spark = rev.CANSparkMax(1, MOTOR_BRUSHLESS)
+        self.boom_rotator_spark = WPI_TalonFX(3)
+
+        self.talon_L_1.setNeutralMode(2)
+        self.talon_L_2.setNeutralMode(2)
+        self.talon_R_1.setNeutralMode(2)
+        self.talon_R_2.setNeutralMode(2)
 
     def disabledPeriodic(self):
         self.sd.putValue("Mode", "Disabled")
@@ -155,7 +157,7 @@ class SpartaBot(MagicRobot):
 
         if self.drive_controller.getRightStickButtonReleased():
             self.solenoid_gear.toggle()
-
+        
 
 if __name__ == '__main__':
     wpilib.run(SpartaBot)
