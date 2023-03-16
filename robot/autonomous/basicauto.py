18b247fbbf2693d046978bd080e035015e468623
Pronkle <allenleiguo@gmail.com>
2023-03-15 22:24:42 -0500

    Added all current autonomous code!

---
+++ robot/autonomous/basicauto.py
@@ -0,0 +1,54 @@
+# This is the Magnolia regional Temporary unfinished autonomous
+# Without any gyroscope abilities or anything. 
+from components.drivetrain import DriveTrain
+from components.grabber import Grabber
+from components.boom import Boom
+from magicbot import magicrobot
+
+from magicbot import AutonomousStateMachine, tunable, state, timed_state
+import networktables
+
+class autoCharge(AutonomousStateMachine):
+    DEFAULT = True
+    MODE_NAME = "autodrive"
+    sd: networktables.NetworkTable
+    drivetrain: DriveTrain
+    boom_arm: Boom
+    solenoid_toggle: Grabber
+    
+    @timed_state(duration = 3, next_state = "ExtendArm",  first = True)
+    def RaiseArm(self):
+        self.boom_arm.set_rotator(-0.15)
+        self.sd.putValue("Mode: ", "Raising Arm")
+    
+    @timed_state(duration = 3, next_state = "Drop")
+    def ExtendArm(self):
+        self.boom_arm.set_rotator(0)
+        self.boom_arm.set_extender(-0.25)
+        self.sd.putValue("Mode: ", "Extending Arm")
+    
+    @timed_state(duration = 3, next_state = "retract")
+    def Drop(self):
+        self.boom_arm.set_extender(0)
+        Grabber.solenoid_toggle()
+        self.sd.putValue("Mode: ", "Dropping")
+
+    @timed_state(duration = 3, next_state = "Moveback")
+    def retract(self):
+        self.boom_arm.set_rotator(0.15)
+        self.boom_arm.set_extender(0.25)
+        self.sd.putValue("Mode: ", "Retracting extender and rotation")
+
+    @timed_state(duration = 1, next_state = "Done")
+    def Moveback(self):
+        self.boom_arm.set_extender(0.0)
+        self.boom_arm.set_rotator(0.0)
+        self.drivetrain(-0.5, 0.0)
+        self.sd.putValue("Mode: ", "going to Charge Station")
+
+    @state
+    def Done(self):
+        self.drivetrain(0.0, 0.0)
+        self.sd.putValue("Mode: ", "Completed!")
+
+
