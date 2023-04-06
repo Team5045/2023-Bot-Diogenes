7564905c6cb16b56fd35da7d297e414b2194d8c3
Pronkle <allenleiguo@gmail.com>
2023-04-06 11:03:10 -0500

    auton recovered + gyro

--- robot/autonomous/basicauto.py
+++ robot/autonomous/basicauto.py
@@ -21,47 +21,67 @@ class Autonomous(AutonomousStateMachine):
     grabber: Grabber
     gyro: Gyro
 
+    @timed_state(duration = 0.75, next_state = "grabit", first = True)
+    def start(self):
+        self.boom_arm.set_rotator(-0.2)
+        self.sd.putValue("Mode: ", "Initial Raise")
     
-    # @timed_state(duration = 3.75, next_state = "pause1",  first = True)
-    # def RaiseArm(self):
-    #     self.boom_arm.set_rotator(-0.2)
-    #     self.sd.putValue("Mode: ", "Raising Arm")
+    @state(next_state = "pause1")
+    def grabit(self):
+        self.boom_arm.set_rotator(0.0)
+        self.grabber.solenoid_toggle()
     
-    # @timed_state(duration = 1, next_state = "ExtendArm")
-    # def pause1(self):
-    #     self.boom_arm.set_rotator(0)
+    @timed_state(duration = 1, next_state = "Rotate")
+    def pause1(self):
+        self.boom_arm.set_rotator(0)
     
-    # @timed_state(duration = 3, next_state = "Drop")
-    # def ExtendArm(self):
-    #     self.boom_arm.set_rotator(0.033)
-    #     self.boom_arm.set_extender(-0.75)
-    #     self.sd.putValue("Mode: ", "Extending Arm")
+    @timed_state(duration = 2.25, next_state = "Drop")
+    def ExtendArm(self):
+        self.boom_arm.set_rotator(-0.2)
+        self.sd.putValue("Mode: ", "Extending Arm")
     
-    # @timed_state(duration = 3, next_state = "retract")
-    # def Drop(self):
-    #     self.boom_arm.set_extender(0) 
-    #     self.grabber.solenoid_toggle()
-    #     self.sd.putValue("Mode: ", "Dropping")
+    @state(next_state = "Retract")
+    def Drop(self):
+        self.boom_arm.set_rotator(0.0) 
+        self.grabber.solenoid_toggle()
+        self.sd.putValue("Mode: ", "Dropping")
 
-    # @timed_state(duration = 3, next_state = "Moveback")
-    # def retract(self):
-    #     self.boom_arm.set_rotator(0.15)
-    #     self.boom_arm.set_extender(0.25)
-    #     self.sd.putValue("Mode: ", "Retracting all previous actions")
+    @timed_state(duration = 3, next_state = "Moveback")
+    def retract(self):
+        self.boom_arm.set_rotator(0.2)
+        self.sd.putValue("Mode: ", "Retracting all previous actions")
 
-    # @timed_state(duration = 1, next_state = "Done")
+    # @timed_state(duration = 3, next_state = "Done")
     # def Moveback(self):
     #     self.boom_arm.set_extender(0.0)
     #     self.boom_arm.set_rotator(0.0)
     #     self.drivetrain.set_motors(-0.5, 0.0)
     #     self.sd.putValue("Mode: ", "Taxi for Points")
+    '''^^^^^^^^^^^^^^^^^^^ THIS IS OPTION ONE WITHOUT GYRO AS IN NO CHARGEPAD ^^^^^^^^^^^^'''
 
-    # @state
-    # def Done(self):
-    #     self.drivetrain.set_motors(0.0, 0.0)
-    #     self.sd.putValue("Mode: ", "Completed!")
+    @timed_state(duration = 2, next_state = "balance")
+    def Moveback(self):
+        self.boom_arm.set_rotator(0.0)
+        self.drivetrain.set_motors(-0.5, 0.0)
+        self.sd.putValue("Mode: ", "Moving Back")
 
+    @timed_state(duration = 6, next_state = "done")
+    def balance(self):
+        self.navx = navx.AHRS.create_spi()
+        if (self.navx.getRoll > 5) and (self.navx.getRoll < -5):
+            self.gyro.balancing()
+            self.sd.putValue("AUTON: ", "GYRO ENABLED")
+        # Does the gyro thing in auton yay
+        else:
+            self.drivetrain.set_motors(0.0, 0.0)
+            self.sd.putValue("AUTON: ", "GYRO NOT DETECTING")
 
+    @state
+    def Done(self):
+        self.drivetrain.set_motors(0.0, 0.0)
+        self.sd.putValue("Mode: ", "Completed!")
+        
+   
     ''' IN THE ASSUMPTION THAT WE DO NOT HAVE A WORKING ARM...'''
     ''' ANYTHING ABOVE IS ONLY IN CASE WE ACTUALLY HAVE AN ARM!!!!!!!!!'''
     '''comment out when you are trying to switch modes'''
@@ -72,27 +92,27 @@ class Autonomous(AutonomousStateMachine):
     #     self.sd.putValue("AUTON: ", "MOVING...")
 
     # Alternative for Chargepad, I suggest using this.
-    @timed_state(duration = 1.75, next_state = "Balancing", first = True)
-    def begin(self):
-        self.drivetrain.set_motors(0.6, 0.0)
-        self.sd.putValue("AUTON: ", "First State...")
+    # @timed_state(duration = 1.75, next_state = "Balancing", first = True)
+    # def begin(self):
+    #     self.drivetrain.set_motors(0.6, 0.0)
+    #     self.sd.putValue("AUTON: ", "First State...")
 
-    @state
-    def balancing(self):
-        self.navx = navx.AHRS.create_spi()
-        if (self.navx.getRoll > 5) and (self.navx.getRoll < -5):
-            self.gyro.balancing()
-            self.sd.putValue("AUTON: ", "GYRO ENABLED")
-        # Does the gyro thing in auton yay
-        else:
-            self.drivetrain.set_motors(0.0, 0.0)
-            self.sd.putValue("AUTON: ", "GYRO NOT DETECTING")
+    # @state
+    # def balancing(self):
+    #     self.navx = navx.AHRS.create_spi()
+    #     if (self.navx.getRoll > 5) and (self.navx.getRoll < -5):
+    #         self.gyro.balancing()
+    #         self.sd.putValue("AUTON: ", "GYRO ENABLED")
+    #     # Does the gyro thing in auton yay
+    #     else:
+    #         self.drivetrain.set_motors(0.0, 0.0)
+    #         self.sd.putValue("AUTON: ", "GYRO NOT DETECTING")
 
-        self.next_state = "COMPLETE"
+    #     self.next_state = "COMPLETE"
     
-    @state
-    def COMPLETE(self):
-        self.drivetrain.set_motors(0.0, 0.0)
-        self.sd.putValue("AUTON: ", "DONE")
+    # @state
+    # def COMPLETE(self):
+    #     self.drivetrain.set_motors(0.0, 0.0)
+    #     self.sd.putValue("AUTON: ", "DONE")
 
 
