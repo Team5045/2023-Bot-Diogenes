c32f83902f9c9af4821a4e6fb83d7bf409a68c72
Pronkle <allenleiguo@gmail.com>
2023-04-07 13:41:48 -0500

    Added config files (controller and constants)

---
+++ robot/config/constants.py
@@ -0,0 +1,40 @@
+'''This is the constants file for our code stuff'''
+
+'''NOTE: Each def. will respresent each component, like drivetrain, gyro, etc... '''
+    
+# This part is for (DRIVETRAIN) --------------------------------------------
+
+SPEED_MULTIPLIER = 1
+ANGLE_MULTIPLIER = 1
+
+# This part is for (GYRO) --------------------------------------------------
+
+MAX_RANGE_LIM_HI = 180
+MAX_RANGE_LIM_LOW = -180
+DEFAULT_LOW = -5
+DEFAULT_HI = 5
+'''NOTE: THIS IS IN DEGREES!'''
+
+# This part is for (BOOM) --------------------------------------------------
+
+STRING_LEN = 28.5  
+'''NOTE: (inches)'''
+
+CHAIN_LEN = 40  
+BUFFER_DISTANCE = 2
+'''NOTE: (encoder ticks)'''
+
+# This part is for the (MAIN FILE)------------------------------------------
+
+SPEED_MULTIPLIER = 1
+ANGLE_MULTIPLIER = 1
+WINDING_SPEED = .2
+INPUT_SENSITIVITY = 0.05
+
+
+
+
+
+
+
+
