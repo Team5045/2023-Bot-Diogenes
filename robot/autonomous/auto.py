c550276b9ff5b78a0e43b699b2477a403ac39c61
Pronkle <allenleiguo@gmail.com>
2023-04-17 20:05:06 -0500

    Auton fixes and SD changes

--- robot/autonomous/auto.py
+++ robot/autonomous/auto.py
@@ -42,6 +42,7 @@ class Autonomous(AutonomousStateMachine):
             # For the Rotator
         self.boom_arm.boom_rotator_motor1.setSelectedSensorPosition(0) # Memphis Pyramid no lai
         self.boom_arm.boom_rotator_motor2.setSelectedSensorPosition(0)
+        self.sd.putValue("Auton_Status: ", "Starting")
         self.next_state("Raise1")
 
     ''' NOTE: TO SELF: ROT_AVG and settarget are USING PLACEHOLDERS, MAKE SURE TO TEST FOR VALUES LATER!!!! '''
@@ -52,16 +53,16 @@ class Autonomous(AutonomousStateMachine):
                    self.boom_arm.boom_rotator_motor2.getSelectedSensorPosition()) / 2
         if ROT_AVG != 0:
             self.boom_controller.set_target(1000)
-            self.sd.putValue("MODE: ", "Initial Rotate")
+            self.sd.putValue("Auton_Status: ", "Initial Rotate")
         else:
             self.boom_controller.set_target(0)
-            self.sd.putValue("MODE: ", "SUCCESS!")
+            self.sd.putValue("Auton_Status: ", "SUCCESS!")
             self.next_state("grab1")
 
     @state
     def grab1(self):
         self.grabber.solenoid_toggle()
-        self.sd.putValue("MODE: ", "Grabbing")
+        self.sd.putValue("Auton_Status: ", "Grabbing")
         self.next_state("Raise2")
 
     @state
@@ -70,34 +71,34 @@ class Autonomous(AutonomousStateMachine):
                    self.boom_arm.boom_rotator_motor2.getSelectedSensorPosition()) / 2
         if ROT_AVG2 != 0:
             self.boom_controller.set_target(0)
-            self.sd.putValue("MODE: ", "Raising Arm")
+            self.sd.putValue("Auton_Status: ", "Raising Arm")
         else:
             self.boom_controller.set_target(0)
-            self.sd.putValue("MODE: ", "Success!")
+            self.sd.putValue("Auton_Status: ", "Success!")
             self.next_state("drop1")
             # NOTE: These statements are a bit janky, but its a redundancy
     
     @state
     def drop1(self):
         self.grabber.solenoid_toggle()
-        self.sd.putValue("MODE: ", "Dropping.")
+        self.sd.putValue("Auton_Status: ", "Dropping.")
         self.next_state("retract")
     
     @state 
     def retract(self):
         self.boom_controller.set_target(2000) # Again, placeholder
         self.grabber.solenoid_toggle() # This prevents grabber damage 
-        self.sd.putValue("MODE: ", "Retracting")
+        self.sd.putValue("Auton_Status: ", "Retracting")
         self.next_state("driveback_1")
 
     @timed_state(duration = 2, next_state = "done")
     def driveback_1(self):
         self.drivetrain.set_motors(-0.6, 0.0)
-        self.sd.putValue("MODE: ", "Driving Back")
+        self.sd.putValue("Auton_Status: ", "Driving Back")
 
     @state
     def done(self):
         self.drivetrain.set_motors(0.0, 0.0)
-        self.sd.puValue("MODE: ", "Auton Done")
+        self.sd.putValue("Auton_Status: ", "Auton Done")
         
     
